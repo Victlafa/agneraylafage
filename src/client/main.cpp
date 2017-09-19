89ee6ba4cd8a00b06d706090b5d9cf062d2ce597
agneray <aurore.agneray@ensea.fr>
2017-09-19 16:41:43 +0200

    MAJ du main

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -16,10 +16,17 @@ using namespace state;
 
 int main(int argc,char* argv[]) 
 {
+    // argc correspond au nombre de mots entres en arguments lors de l'execution incremente de 1.
+    // argv est un tableau de mots dont le premier mot entre par l'utilisateur est l'element d'indice 1.
+    // le tout premier element de argv est le chemin où se trouve le fichier à executer
+    
     Exemple exemple;
     exemple.setX(53);
 
     cout << "It works !" << endl;
+    
+    if (argc >= 2 && (string)argv[1] == "hello")
+        cout << "Bonjour tout le monde !" << endl;
 
     return 0;
 }
