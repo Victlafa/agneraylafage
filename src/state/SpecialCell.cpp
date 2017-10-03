11e5ccb24348e0a2d34a61db348c7d340248351e
lafage <victoire.lafage@ensea.fr>
2017-10-03 14:31:10 +0200

    maj state.dia PlayerRessources -> classe

---
+++ src/state/SpecialCell.cpp
@@ -0,0 +1,25 @@
+#include "SpecialCell.h"
+#include "Cell.h"
+#include "SpecialCellID.h"
+
+
+int SpecialCell::getx(){
+	return x;
+}
+
+void SpecialCell::setx(int new_x){
+	x = new_x;
+}
+
+int SpecialCell::gety(){
+	return y;
+}
+
+void SpecialCell::sety(int new_y){
+	y = new_y;
+}
+
+std::string SpecialCell::getResType(){
+	
+}
+
