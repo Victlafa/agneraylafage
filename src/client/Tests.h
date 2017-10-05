81011a8a8e24c08a82e65d6bdee6e399e1ef62b5
agneray <aurore.agneray@ensea.fr>
2017-10-05 21:04:35 +0200

    Ajout de Tests.h

---
+++ src/client/Tests.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Tests.h
+ * Author: pahtatorr
+ *
+ * Created on 5 octobre 2017, 20:38
+ */
+
+#ifndef TESTS_H
+#define TESTS_H
+
+namespace state
+{
+    void TestsElementTab();
+    void TestsCreaturesGroup();
+    
+}
+
+
+
+#endif /* TESTS_H */
+
