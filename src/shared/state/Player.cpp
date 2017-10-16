233e73576c01545f7887f61e7c3ebf1860afb12e
agneray <aurore.agneray@ensea.fr>
2017-10-16 15:05:56 +0200

    MAJ de ElementTab.cpp et Player.cpp

--- src/shared/state/Player.cpp
+++ src/shared/state/Player.cpp
@@ -96,27 +96,27 @@ namespace state{
         /// Permet de modifier la liste des noms des cellules spéciales que le joueur détient. (ajout ou retrait)
         /// Args d'entrée : nom de la cellule (name) et un booléen (add).
         void Player::modifySpeCellsNames(std::string name, bool add) {
-            size_t initSize = speCellsNames->size();
+            size_t initSize = speCellsNames.size();
 
             // Si on souhaite ajouter un nom dans la liste :
             if (add)
-                speCellsNames->push_back(name);
+                speCellsNames.push_back(name);
 
             // Si au contraire on souhaite retirer un nom de la liste :
-            else if (!add && speCellsNames->size() != 0) {
+            else if (!add && speCellsNames.size() != 0) {
                 
                 // On doit chercher le nom correspondant dans la liste :
 
                 for (int i = 0; i < (int)initSize; i++) {
-                    if (speCellsNames->at(i) == name) {
+                    if (speCellsNames.at(i) == name) {
                         // Quand on a trouvé le bon nom, on le remplace par "" et on sort de la boucle for :
-                        speCellsNames->erase(speCellsNames->begin()+(i-1),speCellsNames->begin()+i);
+                        speCellsNames.erase(speCellsNames.begin()+(i-1),speCellsNames.begin()+i);
                         break;
                     }
                 }
 
                 // Si on a pas trouvé d'index correspondant, on lève une exception :
-                if (speCellsNames->size() == initSize)
+                if (speCellsNames.size() == initSize)
                     throw std::invalid_argument("Aucun nom n'a été supprimé de la liste !");
 
             }
@@ -140,27 +140,27 @@ namespace state{
         }
         
         void Player::setAllCreatures (bool add, CreaturesGroup group){
-            size_t initSize = allCreatures->size();
+            size_t initSize = allCreatures.size();
 
             // Si on souhaite ajouter un groupe de créatures dans la liste :
             if (add)
-                allCreatures->push_back(group);
+                allCreatures.push_back(group);
 
             // Si au contraire on souhaite retirer un groupe de la liste :
-            else if (!add && allCreatures->size() != 0) {
+            else if (!add && allCreatures.size() != 0) {
                 
                 // On doit chercher le groupe correspondant dans la liste :
 
-                for (auto Iter = allCreatures->begin(); Iter != allCreatures->end(); Iter++) {
+                for (auto Iter = allCreatures.begin(); Iter != allCreatures.end(); Iter++) {
                     if (*Iter == group) {
                         // Quand on a trouvé le bon groupe, on le supprime et on sort de la boucle for :
-                        allCreatures->erase(Iter);
+                        allCreatures.erase(Iter);
                         break;
                     }
                 }
 
                 // Si on a pas trouvé d'index correspondant, on lève une exception :
-                if (allCreatures->size() == initSize)
+                if (allCreatures.size() == initSize)
                     throw std::invalid_argument("Aucun groupe n'a été supprimé de la liste !");
 
             }
