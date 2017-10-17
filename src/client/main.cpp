0a09d36e8246f8e528b7c0e89d49b72192a88d81
agneray <aurore.agneray@ensea.fr>
2017-10-17 11:45:33 +0200

    MAJ des Tests et du main

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -37,7 +37,7 @@ int main(int argc,char* argv[])
             TestsCell();
             TestsSimpleCell();
             TestsSpecialCell();
-            //TestsPlayer();
+            TestsPlayer();
             TestsState();
 
         }
