dec9fba1d3226cf4bf7714b73a1b56783f4112eb
agneray <aurore.agneray@ensea.fr>
2017-10-21 11:40:05 +0200

    MAJ main.cpp

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -42,6 +42,11 @@ int main(int argc,char* argv[])
         
         if (argc >= 2 && (string) argv[1] == "render") {
 
+            // On cherche à afficher un seul hexagone.
+            
+            // On créé les six sommets de l'hexagone. Pour cela on doit préciser 
+            // leurs coordonnées dans l'image de sortie et leurs coordonnées dans 
+            // le fichier source de la texture.
             sf::Vertex p1(sf::Vector2f(170,120), sf::Color::White, sf::Vector2f(70,21));
             sf::Vertex p2(sf::Vector2f(221,155), sf::Color::White, sf::Vector2f(121,55));
             sf::Vertex p3(sf::Vector2f(221,206), sf::Color::White, sf::Vector2f(121,106));
@@ -49,29 +54,23 @@ int main(int argc,char* argv[])
             sf::Vertex p5(sf::Vector2f(120,206), sf::Color::White, sf::Vector2f(21,106));
             sf::Vertex p6(sf::Vector2f(120,155), sf::Color::White, sf::Vector2f(21,55));
             
-//            sf::Vertex pt1(sf::Vector2f(70,20), sf::Color::Green, sf::Vector2f(70,20));
-//            sf::Vertex pt2(sf::Vector2f(20,55), sf::Color::Green, sf::Vector2f(20,55));
-//            sf::Vertex pt3(sf::Vector2f(70,55), sf::Color::Green, sf::Vector2f(70,55));
-//            
+            // On créé quatre triangles qui vont nous permettre de former l'hexagone :
             sf::VertexArray t1(sf::Triangles, 3);
             sf::VertexArray t2(sf::Triangles, 3);
             sf::VertexArray t3(sf::Triangles, 3);
             sf::VertexArray t4(sf::Triangles, 3);
-            sf::RenderWindow window(sf::VideoMode(1024,720),"Test Vertex");
+            
+            // On paramètre la fenêtre qui sera affichée en sortie :
+            sf::RenderWindow window(sf::VideoMode(1024,720),"Affichage d'un hexagone :)");
 
-            sf::Texture text1;
-            if(!text1.loadFromFile("./res/hexa.png")) cout<< " erreur chargement text1"<<endl;
-            sf::Texture text2;
-            if(!text2.loadFromFile("./res/hexa.png")) cout<< " erreur chargement text2"<<endl;
-            sf::Texture text3;
-            if(!text3.loadFromFile("./res/hexa.png")) cout<< " erreur chargement text3"<<endl;
-            sf::Texture text4;
-            if(!text4.loadFromFile("./res/hexa.png")) cout<< " erreur chargement text4"<<endl;
+            // Initialisation de la texture de notre hexagone :
+            sf::Texture text;
+            if(!text.loadFromFile("./res/hexa.png")) cout<< " erreur chargement text1"<<endl;
             //sf::RenderStates etat1;
             //etat1.texture = &text1;
             
+            // On associe les triangles aux sommets de l'hexagone :
             t1[0]=p1;t1[1]=p2;t1[2]=p6;
-            //tt1[0]=pt1;tt1[1]=pt2;tt1[2]=pt3;
             t2[0]=p2;t2[1]=p3;t2[2]=p5;
             t3[0]=p2;t3[1]=p5;t3[2]=p6;
             t4[0]=p3;t4[1]=p4;t4[2]=p5;
@@ -83,10 +82,10 @@ int main(int argc,char* argv[])
                     if(event.type==sf::Event::Closed) window.close();
                 }
                 window.clear();
-                window.draw(t1,&text1);
-                window.draw(t2,&text2);
-                window.draw(t3,&text3);
-                window.draw(t4,&text4);
+                window.draw(t1,&text);
+                window.draw(t2,&text);
+                window.draw(t3,&text);
+                window.draw(t4,&text);
                 window.display();
             }
 
