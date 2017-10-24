813b9596c54139faa82be480ea9c90a5b630ff92
agneray <aurore.agneray@ensea.fr>
2017-10-24 11:04:39 +0200

    MAJ de Element.h

--- src/shared/state/Element.h
+++ src/shared/state/Element.h
@@ -4,10 +4,31 @@
 
 
 namespace state {
+  class Element;
+}
+
+
+namespace state {
 
   /// class Element - 
   class Element {
+    // Attributes
+  protected:
+    int x;
+    int y;
+    int creaturesNbr;
+    // Operations
+  public:
+    Element ();
+    virtual ~Element ();
+    bool operator== (Element otherElement);
     // Setters and Getters
+    int getX() const;
+    void setX(int x);
+    int getY() const;
+    void setY(int y);
+    int getCreaturesNbr() const;
+    void setCreaturesNbr(int creaturesNbr);
   };
 
 };
