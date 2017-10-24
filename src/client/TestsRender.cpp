1d8fa5096a867211a9d4eff47e388b922fdde309
agneray <aurore.agneray@ensea.fr>
2017-10-24 09:25:26 +0200

    MAJ de TestsRender.cpp

--- src/client/TestsRender.cpp
+++ src/client/TestsRender.cpp
@@ -13,11 +13,12 @@
 
 #include "TestsRender.h"
 #include "state/State.h"
+#include "render/Layer.h"
 
 namespace render
 {
-//    void TestsStateLayer()
-//    {
+    void TestsStateLayer()
+    {
 //        std::cout << "DEBUT TESTS STATELAYER" << std::endl << std::endl;
 //
 //        std::unique_ptr<StateLayer> stLayer(new StateLayer(new state::State()));
@@ -26,10 +27,28 @@ namespace render
 //        std::cout << ((NULL != stLayer) ? "OK" : "KO") << std::endl;
 //        
 //        std::cout << "Test StateLayer - Getter/Setter surface : ";
-//        stLayer.setSurface(new Surface());
+//        stLayer//.setSurface(new Surface());
 //        std::cout << ((NULL != stLayer.getSurface()) ? "OK" : "KO") << std::endl;
 //        
 //        std::cout << "Test StateLayer - Init state : ";
 //        std::cout << ((NULL != stLayer.state) ? "OK" : "KO") << std::endl;
-//    }
+    }
+    
+    void TestsElementTabLayer()
+    {
+        std::cout << "DEBUT TESTS ELEMENTTABLAYER" << std::endl << std::endl;
+        
+        std::unique_ptr<state::ElementTab> grid(new state::ElementTab(7, 5));
+        ElementTabLayer gridLayer(*grid);
+        gridLayer.initSurface(0);
+
+        std::cout << "Test ElementTabLayer - Init ElementTabLayer : ";
+        std::cout << ((NULL != gridLayer) ? "OK" : "KO") << std::endl;
+
+        std::cout << "Test ElementTabLayer - Getter/Setter surface : ";
+        gridLayer.setSurface(new Surface());
+        std::cout << ((NULL != gridLayer.getSurface()) ? "OK" : "KO") << std::endl;
+        
+        
+    }
 }
