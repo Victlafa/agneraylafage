b5bf62ba3565728013f26d7f5908775f6762bb82
agneray <aurore.agneray@ensea.fr>
2017-10-24 16:23:31 +0200

    MAJ Surface.cpp

--- src/shared/render/Surface.cpp
+++ src/shared/render/Surface.cpp
@@ -12,10 +12,24 @@ namespace render
     {
         if(!this->texture.loadFromFile("./res/" + imgFile)) 
             std::cout<< "Erreur chargement texture de Surface ! \n"<< std::endl;
+        
     }
     
-    void Surface::setSpriteLocation (int index, int x, int y){ }
-    void Surface::setSpriteTexture (int index, const Tile& tex){ }
+    void Surface::setSpriteLocation (int index, int x, int y)
+    { 
+        this->quadsList[i][0].position = sf::Vector2f(260 + 102 * i, 126);
+        this->quadsList[i][1].position = sf::Vector2f(361 + 102 * i, 126);
+        this->quadsList[i][2].position = sf::Vector2f(260 + 102 * i, 248);
+        this->quadsList[i][3].position = sf::Vector2f(361 + 102 * i, 248);
+    }
+    
+    void Surface::setSpriteTexture (int index, const Tile& tex)
+    { 
+        this->quadsList[index][0].texCoords = sf::Vector2f(x - 51,y + 61);
+        this->quadsList[index][1].texCoords = sf::Vector2f(x + 51,y + 61);
+        this->quadsList[index][2].texCoords = sf::Vector2f(x - 51,y - 61);
+        this->quadsList[index][3].texCoords = sf::Vector2f(x + 51,y - 61);
+    }
     
     void Surface::draw (sf::RenderTarget& target, sf::RenderStates states) const
     {
@@ -28,6 +42,8 @@ namespace render
         this->quadsList.reserve(count);
         for (int i = 0; i < count; i ++)
         {
+            // Preparation de la première ligne d'hexagones  :
+            this->quadsList.push_back(sf::VertexArray(sf::Quads,4));
             
         }
     }
