96acbe663aa7dc9f50bd64d4e3ff0b74beaeb9ee
agneray <aurore.agneray@ensea.fr>
2017-10-24 11:38:54 +0200

    Ajout de Surface.cpp

---
+++ src/shared/render/Surface.cpp
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include "Surface.h"
+
+namespace render
+{
+    void Surface::loadTexture (const std::string& imgFile)
+    {
+        if(!this->texture.loadFromFile("./res/" + imgFile)) 
+            std::cout<< "Erreur chargement texture de Surface ! \n"<< std::endl;
+    }
+    
+    void Surface::setSpriteLocation (int index, int x, int y){ }
+    void Surface::setSpriteTexture (int index, const Tile& tex){ }
+//    void Surface::draw (sf::RenderTarget& target, sf::RenderStates states) const { }
+    void Surface::initQuads (int count)
+    {
+        this->quadsList.reserve(count);
+    }
+//    // Setters and Getters
+    const sf::Texture& Surface::getTexture() const
+    {
+        return this->texture;
+    }
+    void Surface::setTexture(const sf::Texture& texture)
+    {
+        this->texture = texture;
+    }
+    const std::vector<sf::VertexArray>& Surface::getQuadsList() const
+    {
+        return this->quadsList;
+    }
+    void Surface::setQuadsList(const std::vector<sf::VertexArray>& quadsList)
+    {
+        this->quadsList = quadsList;
+    }
+}
