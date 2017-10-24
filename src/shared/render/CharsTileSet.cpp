d38aff82c1301a18864cf58053c3a2202c37f9d6
agneray <aurore.agneray@ensea.fr>
2017-10-24 09:25:55 +0200

    Ajout de CharsTileSet.cpp

---
+++ src/shared/render/CharsTileSet.cpp
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include "CharsTileSet.h"
+#include "state/TypeID.h"
+
+namespace render
+{
+    CharsTileSet::CharsTileSet (){}
+    
+    int CharsTileSet::getCellWidth () const
+    {
+        if (this->creaturesPlayer1.size() != 0)
+            return this->creaturesPlayer1[0].getWidth();
+        else
+            return -1;
+    }
+    
+    int CharsTileSet::getCellHeight () const
+    {
+        if (this->creaturesPlayer1.size() != 0)
+            return this->creaturesPlayer1[0].getHeight();
+        else
+            return -1;
+    }
+    
+    const std::string CharsTileSet::getImageFile () const{
+        return "./res/groupes.png";
+    }
+    
+    const Tile& CharsTileSet::getTile (const state::ElementTab& elemTab) const
+    {
+        Tile tuile;
+        
+        if (elemTab.getTabType() == state::TypeID::CREATURESGROUP) {
+            tuile = Tile(0,0,100,100);
+        } else
+            tuile = Tile();
+        
+        return tuile;
+    }
+}
+
