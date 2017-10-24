c7af53a6a07365ba58d7fe65ae8a9a4c2c3a90f2
agneray <aurore.agneray@ensea.fr>
2017-10-24 08:41:28 +0200

    MAJ de CreaturesSurface.cpp

--- src/shared/render/CreaturesSurface.cpp
+++ src/shared/render/CreaturesSurface.cpp
@@ -9,10 +9,20 @@
 namespace render
 {
     void CreaturesSurface::loadTexture (const std::string& imgFile){
-        if(!this->texture.loadFromFile("./res/hexa.png")) cout<< " erreur chargement text1"<<endl;
+        if(!this->texture.loadFromFile("./res/" + imgFile)) 
+            std::cout << "Erreur chargement texture de CreaturesSurface" << std::endl;
     }
-    void CreaturesSurface::setSpriteLocation (int index, int x, int y);
-    void CreaturesSurface::setSpriteTexture (int index, const Tile& tex);
+    
+    void CreaturesSurface::setSpriteLocation (int index, int x, int y){
+        //this->quadsList.at(index)
+    }
+    void CreaturesSurface::setSpriteTexture (int index, const Tile& tex)
+    {
+        
+    }
     void CreaturesSurface::draw (sf::RenderTarget& target, sf::RenderStates states) const;
-    void CreaturesSurface::initQuads (int count);
+    void CreaturesSurface::initQuads (int count)
+    {
+        
+    }
 }
