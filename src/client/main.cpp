53c0dea4e27808b8baf5360b7b4ce5b1580bd743
agneray <aurore.agneray@ensea.fr>
2017-10-25 10:21:18 +0200

    MAJ de main.cpp - suppression des lignes commentees

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -47,74 +47,10 @@ int main(int argc,char* argv[])
         
         if (argc >= 2 && (string) argv[1] == "render") {
 
-//            // On cherche à afficher un seul hexagone.
-//            
-//            // On créé les six sommets de l'hexagone. Pour cela on doit préciser 
-//            // leurs coordonnées dans l'image de sortie et leurs coordonnées dans 
-//            // le fichier source de la texture.
-//            sf::Vertex p1(sf::Vector2f(170,120), sf::Vector2f(70,21));
-//            sf::Vertex p2(sf::Vector2f(221,155), sf::Vector2f(121,55));
-//            sf::Vertex p3(sf::Vector2f(221,206), sf::Vector2f(121,106));
-//            sf::Vertex p4(sf::Vector2f(170,241), sf::Vector2f(70,141));
-//            sf::Vertex p5(sf::Vector2f(120,206), sf::Vector2f(21,106));
-//            sf::Vertex p6(sf::Vector2f(120,155), sf::Vector2f(21,55));
-//            
-//            // On créé quatre triangles qui vont nous permettre de former l'hexagone :
-//            sf::VertexArray t1(sf::Triangles, 3);
-//            sf::VertexArray t2(sf::Triangles, 3);
-//            sf::VertexArray t3(sf::Triangles, 3);
-//            sf::VertexArray t4(sf::Triangles, 3);
-//            
-//            // On paramètre la fenêtre qui sera affichée en sortie :
-//            sf::RenderWindow window(sf::VideoMode(1024,720),"Affichage d'un hexagone :)");
-//
-//            // Initialisation de la texture de notre hexagone :
-//            sf::Texture text;
-//            if(!text.loadFromFile("./res/hexa.png")) cout<< " erreur chargement text1"<<endl;
-//            
-//            // On associe les triangles aux sommets de l'hexagone :
-//            t1[0]=p1;t1[1]=p2;t1[2]=p6;
-//            t2[0]=p2;t2[1]=p3;t2[2]=p5;
-//            t3[0]=p2;t3[1]=p5;t3[2]=p6;
-//            t4[0]=p3;t4[1]=p4;t4[2]=p5;
-//
-//            sf::Font myFont;
-//            if (!myFont.loadFromFile("./res/HPS_Extrabold_trial.ttf"))
-//                std::cout << "Erreur chargement police\n" << std::endl;
-//            
-//            sf::Text message("Bonjour !",myFont,80.f);
-//            message.setStyle(sf::Text::Bold);
-//            message.setColor(sf::Color::White);
-//            message.setPosition(300,300);
-//            
-//            sf::VertexArray vertexA(sf::LinesStrip,7);
-//            vertexA[0] = p1;
-//            vertexA[1] = p2;
-//            vertexA[2] = p3;
-//            vertexA[3] = p4;
-//            vertexA[4] = p5;
-//            vertexA[5] = p6;
-//            vertexA[6] = p1;
-//            
-//            sf::CircleShape hexagone(120,6);
-//            hexagone.setTexture(&text,false);
-//            
-//            while(window.isOpen()){
-//                sf::Event event;
-//                while(window.pollEvent(event)){
-//                    if(event.type==sf::Event::Closed) window.close();
-//                }
-//                window.clear();
-//                window.draw(t1,&text);
-//                window.draw(t2,&text);
-//                window.draw(t3,&text);
-//                window.draw(t4,&text);
-//                //window.draw(vertexA,&text);
-//                //window.draw(hexagone);
-//                window.draw(message);
-//                window.display();
-//            }
-            TestsElementTabLayer();
+            // On cherche à afficher une ligne de cinq hexagones :
+            //TestsElementTabLayer();
+            
+            
 
         }
         
