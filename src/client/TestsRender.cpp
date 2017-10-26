7dbaad7ab7d3062a2c816b5144be0ac8d523f379
lafage <victoire.lafage@ensea.fr>
2017-10-26 21:38:46 +0200

    MAJ testmap compilation + affichage

--- src/client/TestsRender.cpp
+++ src/client/TestsRender.cpp
@@ -76,9 +76,22 @@ namespace render {
     {
         
         std::cout << "Initialisation map_cell & map_creatures" << std::endl;
-        state::ElementTab map_cell(state::TypeID::CELL,(size_t)5,(size_t)7);
-        state::ElementTab map_creatures(state::TypeID::CREATURESGROUP,(size_t)5,(size_t)7);
+        //state::ElementTab map_cell(state::TypeID::CELL,(size_t)5,(size_t)7);
+        //state::ElementTab map_creatures(state::TypeID::CREATURESGROUP,(size_t)5,(size_t)7);
         
+        sf::RenderWindow window(sf::VideoMode(1024, 720), "Garden Tensions"); //, sf::Style::Close | sf::Style::Titlebar);
+
+        int halfHeight = 61;
+        int halfWidth = 51;
+        int x = 311;
+        int y = 187;
+        int xText = 71;
+        int yText = 81;
+        int shift = 0;
+        
+        std::map<int,state::Cell> map_cell;
+        std::map<int,int> map_cell_text;
+
         
         std::cout << "Positionnement des Cellules Spéciales" << std::endl;
         //Positionnement des Cellules Spéciales
@@ -112,6 +125,8 @@ namespace render {
             li[sc]=i;lj[sc]=j;
             
             //map_cell(i,j)=*spec;
+            map_cell[i*7+j]=*spec;
+            map_cell_text[i*7+j]=sc+3;
             
             std::cout << i << std::endl;
             std::cout << j << std::endl;
@@ -127,12 +142,132 @@ namespace render {
                 if(!((i==0&&j==0) || (i==0&&j==1) || (i==1&&j==0) || (i==4&&j==6) || (i==4&&j==5) || (i==3&&j==6) || (i==li[0]&&j==lj[0]) || (i==li[1]&&j==lj[1]) || (i==li[2]&&j==lj[2]) || (i==li[3]&&j==lj[3]))){
                     std::string restype = "wood";
                     //map_cell(i,j)= state::SimpleCell(l_SimCID[rand()%3],restype,4,i,j);
+                    int iid = rand()%3;
+                    state::SimpleCellID id = l_SimCID[iid];
+                    map_cell[i*7+j]=state::SimpleCell(id,restype,4,i,j);
+                    map_cell_text[i*7+j]=iid;
                 }
                 //else
                     //map_cell(i,j) = NULL;
             }
         }
         
+
+        
+        std::vector<sf::VertexArray> listHexagones = std::vector<sf::VertexArray>();
+        listHexagones.reserve(29);
+        
+        for (int i = 0; i < 29; i++) {
+
+            listHexagones.push_back(sf::VertexArray(sf::Quads, 4));
+            
+            if (i == 5 || i == 11) {
+                x -= halfWidth;
+                y += 86;
+                switch (i) {
+                    case 5:
+                        shift = i - 5;
+                        break;
+                    case 11:
+                        shift = i - 11;
+                        break;
+                    default:
+                        shift = i;
+                }
+            }
+                
+            else if (i == 18 || i == 24) {
+                
+                x += halfWidth;
+                y += 86;
+            switch (i) {
+                    case 18:
+                        shift = i - 18;
+                        break;
+                    case 24:
+                        shift = i - 24;
+                        break;
+                    default:
+                        shift = i;
+                }
+            }
+                
+            else
+            {
+                shift = shift;
+            }
+
+            listHexagones[i][0].position = sf::Vector2f(x + halfWidth + shift * 2 * halfWidth, y + halfHeight);
+            listHexagones[i][1].position = sf::Vector2f(x + halfWidth + shift * 2 * halfWidth, y - halfHeight);
+            listHexagones[i][2].position = sf::Vector2f(x - halfWidth + shift * 2 * halfWidth, y - halfHeight);
+            listHexagones[i][3].position = sf::Vector2f(x - halfWidth + shift * 2 * halfWidth, y + halfHeight);
+
+            switch(map_cell_text[i]){
+                case 6 :
+                    xText=71;
+                    yText=221;
+                    break;
+                case 5 :
+                    xText=191;
+                    yText=221;
+                    break;
+                case 4 :
+                    xText=311;
+                    yText=221;
+                    break;
+                case 3 :
+                    xText=431;
+                    yText=221;
+                    break;
+                case 2 :
+                    xText=311;
+                    yText=81;
+                    break;
+                case 1 :
+                    xText=191;
+                    yText=81;
+                    break;
+                case 0 :
+                    xText=71;
+                    yText=81;
+                    break;
+                default:
+                    xText=71;
+                    yText=81;
+                    break;
+            }
+            
+            
+            listHexagones[i][0].texCoords = sf::Vector2f(xText + halfWidth, yText + halfHeight);
+            listHexagones[i][1].texCoords = sf::Vector2f(xText + halfWidth, yText - halfHeight);
+            listHexagones[i][2].texCoords = sf::Vector2f(xText - halfWidth, yText - halfHeight);
+            listHexagones[i][3].texCoords = sf::Vector2f(xText - halfWidth, yText + halfHeight);
+
+            shift += 1;
+
+        }
+        
+        while (window.isOpen()) {
+            sf::Event event;
+            while (window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed) window.close();
+            }
+
+            window.clear();
+
+            sf::Texture hexaTexture;
+
+            //Le premier cas marche chez Victoire, le second chez Aurore
+            if (!hexaTexture.loadFromFile("../res/hexa.png"))
+                std::cout << "Erreur chargement texture !\n" << std::endl;
+            //throw std::runtime_error("Impossible de lire le fichier");
+          
+            
+            for (int i = 0; i < 29; i++)
+                window.draw(listHexagones[i], &hexaTexture);
+
+            window.display();
+        }
         
         std::cout << "Initialisation map_creature" << std::endl;
         //On initialise map_creature
