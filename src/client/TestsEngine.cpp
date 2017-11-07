015cfe1fa0978a17109e9e9864ff58ffffd621c7
agneray <aurore.agneray@ensea.fr>
2017-11-07 22:25:17 +0100

    MAJ de TestsEngine.cpp - il n'y a plus d'erreur de segmentation !

--- src/client/TestsEngine.cpp
+++ src/client/TestsEngine.cpp
@@ -19,12 +19,14 @@ namespace engine {
         
         // On cree un etat
         state::State etat;
+        // On cree un moteur 
+        engine::Engine moteur;
         
         // On place un forgeron dans la case (4,2)
         etat.getCharacters()->set(new state::CreaturesGroup(state::CreaturesID::BLACKSMITH),4,2);
         // On place un cuisinier dans la case (4,3)
         etat.getCharacters()->set(new state::CreaturesGroup(state::CreaturesID::COOKER),4,3);
-        
+        std::cout << "Adresse du joueur 1 : " << etat.getPlayer(1).get() << std::endl;
         // On cree une cellule en (4,3)
         etat.getGrid()->set(new state::Cell(state::CellTypeID::SIMPLE),4,3);
         
@@ -36,9 +38,6 @@ namespace engine {
         
         // On souhaite que le joueur 1 attaque le joueur 2
         etat.getPlayer(1)->setIsStriker(true);
-        
-        // On cree un moteur en lui associant l'etat defini precedemment
-        engine::Engine moteur;
         
         // On ajoute une commande au moteur
         moteur.addCommand(1,new FightCommand(4*7 + 2 - 1, 4*7 + 3 - 1, 4*7 + 3 - 1));
