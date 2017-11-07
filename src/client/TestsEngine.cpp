6c7a1b3f65878bbee8071208a1e2832442b5ad66
agneray <aurore.agneray@ensea.fr>
2017-11-07 09:50:12 +0100

    Ajout de TestsEngine

---
+++ src/client/TestsEngine.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   TestsEngine.cpp
+ * Author: pahtatorr
+ * 
+ * Created on 7 novembre 2017, 09:35
+ */
+
+#include "TestsEngine.h"
+#include "state/State.h"
+
+namespace engine {
+    
+    void TestsFightCommand(){
+        
+        // On cree un etat :
+        state::State etat = state::State();
+    }
+}
