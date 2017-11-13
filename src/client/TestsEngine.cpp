9aecb760b8759b709529f5653053f3f10e7d186a
agneray <aurore.agneray@ensea.fr>
2017-11-13 16:21:08 +0100

    MAJ de TestsEngine.cpp et de state.h

--- src/client/TestsEngine.cpp
+++ src/client/TestsEngine.cpp
@@ -19,26 +19,32 @@ namespace engine {
         
         // On cree un moteur qui creera automatiquement un etat
         engine::Engine moteur;
-        
-        // On place un forgeron dans la case (4,2)
-        moteur.getState().getCharacters()->set(new state::CreaturesGroup(state::CreaturesID::BLACKSMITH),4,2);
-        // On place un cuisinier dans la case (4,3)
-        moteur.getState().getCharacters()->set(new state::CreaturesGroup(state::CreaturesID::COOKER),4,3);
-        std::cout << "Adresse du joueur 1 : " << moteur.getState().getPlayer(1).get() << std::endl;
+        int ligneCombat = 2;
+        int colonne1 = 3;
+        int colonne2 = 4;
+        // On place un forgeron
+        std::cout << "Creation d'un forgeron" << std::endl;
+        moteur.getState().getCharacters()->set(new state::CreaturesGroup(state::CreaturesID::BLACKSMITH),ligneCombat,colonne1);
+        // On place un cuisinie dans la case (4,3)
+        std::cout << "Creation d'un cuisinier" << std::endl;
+        moteur.getState().getCharacters()->set(new state::CreaturesGroup(state::CreaturesID::COOKER),ligneCombat,colonne2);
         // On cree une cellule en (4,3)
-        moteur.getState().getGrid()->set(new state::Cell(state::CellTypeID::SIMPLE),4,3);
+        
+        //moteur.getState().getGrid()->set(new state::Cell(state::CellTypeID::SIMPLE),4,3);
         
         // On associe le numero du forgeron au joueur 1
-        moteur.getState().getPlayer(1)->setAllCreatures(true, 4*7 + 2 - 1);
+        moteur.getState().getPlayer(1)->setAllCreatures(true, ligneCombat*7 + colonne1 - 1);
+        std::cout << "Creature presente en (2,3) : " << moteur.getState().getPlayer(1)->getAllCreatures().at(ligneCombat*7 + colonne1 - 1) << std::endl;
         
         // On associe le numero du cuisinier au joueur 2
-        moteur.getState().getPlayer(2)->setAllCreatures(true, 4*7 + 3 - 1);
-        
+        moteur.getState().getPlayer(2)->setAllCreatures(true, ligneCombat*7 + colonne2 - 1);
+        std::cout << "Creature presente en (2,4) : " << moteur.getState().getPlayer(2)->getAllCreatures().at(ligneCombat*7 + colonne2 - 1) << std::endl;
+
         // On souhaite que le joueur 1 attaque le joueur 2
         moteur.getState().getPlayer(1)->setIsStriker(true);
         
         // On ajoute une commande au moteur
-        moteur.addCommand(1,new FightCommand(4*7 + 2 - 1, 4*7 + 3 - 1, 4*7 + 3 - 1));
+        moteur.addCommand(1,new FightCommand(ligneCombat*7 + colonne1 - 1, ligneCombat*7 + colonne2 - 1, ligneCombat*7 + colonne2 - 1));
         // On execute les commandes
         moteur.update();
         // On affiche les points de conquete des deux joueurs 
