47c5692dffd4217da36182184574051f2113b421
lafage <victoire.lafage@ensea.fr>
2017-11-16 21:13:32 +0100

    MAJ ajout de l'ia

---
+++ src/client/TestRandomIA.h
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   TestRandomIA.h
+ * Author: mikhan
+ *
+ * Created on 16 novembre 2017, 21:05
+ */
+
+#ifndef TESTRANDOMIA_H
+#define TESTRANDOMIA_H
+
+#include "../shared/state.h"
+#include "../shared/engine.h"
+#include "../shared/ai.h"
+#include <iostream>
+
+namespace ai{
+    void TestsFightCommand();
+    
+}
+
+#endif /* TESTRANDOMIA_H */
+
