4d3045219e3783e5c0864aeae4ccad0f9ebbd4f6
agneray <aurore.agneray@ensea.fr>
2017-11-25 18:26:49 +0100

    Ajout d'une classe (Game) pour gerer l'initialisation et le lancement du jeu

---
+++ src/client/Game.cpp
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Game.cpp
+ * Author: pahtatorr
+ * 
+ * Created on 25 novembre 2017, 18:12
+ */
+
+#include "Game.h"
+
+void Start()
+{
+    int type;
+    
+    // On demande au joueur 1 quel type de creatures il souhaite jouer
+    std::cout << "Quel type de creatures souhaitez-vous jouer ?\n" << std::endl;
+    std::cout << "Cuisinier : 1\nForgeron : 2\nBûcheron : 3\nMineur : 4\n" << std::endl;
+    std::cin >> type;
+    
+    // On initialise le moteur de jeu
+    Engine moteur(((CreaturesID)(type)));
+    
+}
---
+++ src/client/Game.h
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Game.h
+ * Author: pahtatorr
+ *
+ * Created on 25 novembre 2017, 18:12
+ */
+
+#ifndef GAME_H
+#define GAME_H
+
+#include "../shared/state.h"
+#include "../shared/engine.h"
+
+using namespace engine;
+using namespace state;
+
+void Start();
+
+#endif /* GAME_H */
