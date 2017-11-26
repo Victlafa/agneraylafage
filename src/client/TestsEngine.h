936269bc73c5c43452c4756ef7cc20290d722405
agneray <aurore.agneray@ensea.fr>
2017-11-26 21:56:54 +0100

    MAJ des tests et de main

--- src/client/TestsEngine.h
+++ src/client/TestsEngine.h
@@ -24,6 +24,7 @@ namespace engine{
     void TestsPoisonCommand();
     void TestsNewGameCommand();
     void TestsMoveCommand();
+    void TestsSpecialCommand();
 }
 
 #endif /* TESTSENGINE_H */
--- src/client/main.cpp
+++ src/client/main.cpp
@@ -75,12 +75,18 @@ int main(int argc,char* argv[])
             //TestsMoveCharCommand();
             //TestsPoisonCommand();
             //TestsNewGameCommand();
-            TestsMoveCommand();
+            //TestsMoveCommand();
+            TestsSpecialCommand();
         }
         
         else if (argc >= 2 && (string) argv[1] == "random_ai") {
             
-            //TestsRandomIA();
+            TestsRandomIA();
+        
+        }
+        
+        else if (argc >= 2 && (string) argv[1] == "heuristic_ai") {
+            
             TestsHeuristicIA();
         
         }
