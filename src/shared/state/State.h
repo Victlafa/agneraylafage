38f9273d91f423bac8dca72d4d64f0cc5088f601
agneray <aurore.agneray@ensea.fr>
2017-12-04 16:17:40 +0100

    MAJ de l'état

--- src/shared/state/State.h
+++ src/shared/state/State.h
@@ -29,6 +29,7 @@ namespace state {
     std::unique_ptr<Player> player1;
     std::unique_ptr<Player> player2;
     int totalCellNbr;
+    int freeCellNbr;
     // Operations
   public:
     State (CreaturesID typePl1);
@@ -36,6 +37,7 @@ namespace state {
     const std::unique_ptr<CellTab>& getGrid () const;
     const std::unique_ptr<CreaturesTab>& getCharacters () const;
     int getCellNbr () const;
+    int getFreeCellNbr () const;
     const std::unique_ptr<Player>& getPlayer (int number) const;
     void reset (CreaturesID typeCreatures);
     void initCreatures ();
