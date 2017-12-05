67f450b21d9735b021cfbb092e45adce4e6c2145
lafage <victoire.lafage@ensea.fr>
2017-12-05 10:25:10 +0100

    Ajout PoisonAction.h & .cpp

---
+++ src/shared/engine/PoisonAction.cpp
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include "PoisonAction.h"
+#include "state/Cell.h"
+#include <iostream>
+
+using namespace state;
+
+namespace engine
+{
+    PoisonAction::PoisonAction (int i_cell, int j_cell) : cell(2) {
+        cell[0] = i_cell;
+        cell[1] = j_cell;
+    }
+    
+    bool PoisonAction::isPoisoned (state::State& state){
+        bool isPoison = state.getGrid()->isPoisoned(cell[0],cell[1]);
+        if (isPoison)
+            return true;
+        else
+            return false;
+    }
+    
+    void PoisonAction::killCreatures (state::State& state){
+        srand(time(NULL));
+        int nbCreaTuees = 0;
+        
+        if (isPoisoned(state)) {
+            int tirageJeu = rand() % 6 + 1;
+            int nbCrea = state.getCharacters()->get(cell[0], cell[1])->getCreaturesNbr();
+            for (int i = 0; i < nbCrea; i++) {
+                if (tirageJeu > rand() % 6 + 1)
+                    nbCreaTuees += 1;
+            }
+
+            state.getCharacters()->get(cell[0], cell[1])->setCreaturesNbr(nbCrea - nbCreaTuees);
+        }
+        else
+            std::cout << "La cellule n'est pas empoisonnee, pas d'effet" << std::endl;
+         
+    }
+    
+    CommandTypeID PoisonAction::getTypeID () const {return CommandTypeID::POISON;}
+    void PoisonAction::execute (state::State& state){
+        killCreatures(state);
+    }
+    // Setters and Getters
+    const std::vector<int>& PoisonAction::getCell() const{return cell;}
+    void PoisonAction::setCell(const std::vector<int>& cell){this->cell = cell;}
+}
