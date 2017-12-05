7599be1532d42320f9ead81f90d98d0c8139f60e
lafage <victoire.lafage@ensea.fr>
2017-12-05 10:25:40 +0100

    Ajout ProtectedAction.h & .cpp

---
+++ src/shared/engine/ProtectedAction.cpp
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include "ProtectedAction.h"
+#include "../state.h"
+#include <iostream>
+
+using namespace state;
+
+namespace engine
+{
+    ProtectedCommand::ProtectedAction (int i_cell, int j_cell) : cell(2) {
+        cell[0] = i_cell;
+        cell[1] = j_cell;
+    };
+    
+    bool ProtectedAction::isProtected(state::State& state){
+        CellState etatCell = ((Cell)*state.getGrid()->get(cell[0],cell[1])).getCellState();
+        std::cout << "etat de la cellule : " << etatCell << std::endl;
+        if (etatCell == CellState::PROTECTED)
+            return true;
+        else
+            return false;
+    }
+    
+    void ProtectedAction::protectProcess (state::State& state)
+    {
+        
+    }
+    
+    CommandTypeID ProtectedAction::getTypeID () const {return CommandTypeID::PROTECTED; }
+    void ProtectedAction::execute (state::State& state)
+    {
+        protectProcess(state);
+    }
+    // Setters and Getters
+    const std::vector<int>& ProtectedAction::getCell() const{return cell;}
+    void ProtectedAction::setCell(const std::vector<int>& cell) {this->cell = cell;}
+}
