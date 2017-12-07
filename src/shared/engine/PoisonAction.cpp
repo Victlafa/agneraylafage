70eb959798e0d39f9ada765364d70ea1d818a5c9
lafage <victoire.lafage@ensea.fr>
2017-12-07 15:24:07 +0100

    MAJ PoisonAction.cpp undo poison

--- src/shared/engine/PoisonAction.cpp
+++ src/shared/engine/PoisonAction.cpp
@@ -6,15 +6,19 @@
 
 #include "PoisonAction.h"
 #include "state/Cell.h"
+#include "state/CreaturesGroup.h"
 #include <iostream>
 
 using namespace state;
 
 namespace engine
 {
-    PoisonAction::PoisonAction (int i_cell, int j_cell) : cell(2) {
+    PoisonAction::PoisonAction (int i_cell, int j_cell, state::ID type, int player) : cell(2) {
         cell[0] = i_cell;
         cell[1] = j_cell;
+        nbrCreaBegin = -1;
+        typeCreaBegin = type;
+        playerBegin = player;
     }
     
     bool PoisonAction::isPoisoned (state::State& state){
@@ -28,6 +32,9 @@ namespace engine
     void PoisonAction::killCreatures (state::State& state){
         srand(time(NULL));
         int nbCreaTuees = 0;
+        
+        nbrCreaBegin = state.getCharacters()->get(cell[0], cell[1])->getCreaturesNbr();
+        typeCreaBegin = state.getCharacters()->get(cell[0], cell[1])->getElemType();
         
         if (isPoisoned(state)) {
             int tirageJeu = rand() % 6 + 1;
@@ -48,9 +55,14 @@ namespace engine
         killCreatures(state);
     }
     
-    void PoisonAction::undo (state::State& etat)
+    void PoisonAction::undo (state::State& state)
     {
-        
+        if(state.getCharacters()->get(cell[0], cell[1])->getCreaturesNbr()==0){
+           std::unique_ptr<state::Element> crea(new CreaturesGroup(this->typeCreaBegin, this->nbrCreaBegin, state.getCharacters()->get(cell[0],cell[1])->getPlayer()));
+           state.getCharacters()->set(crea.get(),cell[0],cell[1]);
+        }
+        else
+            state.getCharacters()->get(cell[0], cell[1])->setCreaturesNbr(nbrCreaBegin);
     }
     
     // Setters and Getters
