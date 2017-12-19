3276f0728449e81512cd98e467b5945fce4d973d
lafage <victoire.lafage@ensea.fr>
2017-12-19 17:20:20 +0100

    AJout server/ api

---
+++ src/server/server/User.cpp
@@ -0,0 +1,14 @@
+/** 
+ * @file User.cpp
+ * @author Philippe-Henri Gosselin
+ * @date 9 décembre 2015
+ * @copyright CNRS
+ */
+
+#include "User.hpp"
+#include "User.h"
+using namespace server;
+
+User::User (std::string name, int id) : name(name),id(id) {
+    
+}
