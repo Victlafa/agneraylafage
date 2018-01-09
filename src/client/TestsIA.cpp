2754183e7fd1c47fe4bb37fb19b94e3466a2fc7d
agneray <aurore.agneray@ensea.fr>
2018-01-09 12:06:25 +0100

    MAJ de TestsIA - j'ai commenté les methodes liees a l'utilisation des threads

--- src/client/TestsIA.cpp
+++ src/client/TestsIA.cpp
@@ -14,12 +14,12 @@ using namespace std;
 namespace ai {
     
     // Pour gestion multi-thread
-    mutex notre_mutex;
-    int tour = 0;
-    // numero du joueur qui commence en premier
-    int beginner = 0;
-    // Declaration de la fenetre
-    sf::RenderWindow gameWindow(sf::VideoMode(1024, 720), "Garden Tensions"); //, sf::Style::Close | sf::Style::Titlebar);
+//    mutex notre_mutex;
+//    int tour = 0;
+//    // numero du joueur qui commence en premier
+//    int beginner = 0;
+//    // Declaration de la fenetre
+//    sf::RenderWindow gameWindow(sf::VideoMode(1024, 720), "Garden Tensions"); //, sf::Style::Close | sf::Style::Titlebar);
 
     void TestsRandomIA() 
     {
@@ -237,115 +237,115 @@ namespace ai {
         std::cout << "\nNotre démonstration est terminée :)" << std::endl;
     }
     
-    void TestsThread()
-    {
-        // On prend possession du mutex
-        notre_mutex.lock();
-        srand(time(NULL));
-        
-        // On initialise un moteur, on choisit les mineurs pour le joueur 1
-        engine::Engine moteur(CreaturesID::MINERS);
-        // On initialise une ia
-        HeuristicAI ia(&moteur, rand()%30000);
-        
-        // On crée un Layer qui permettra de gerer l'affichage des cellules
-        render::CellTabLayer cellLayer(*(moteur.getState().getGrid().get()));
-        // On crée un Layer qui permettra de gerer l'affichage des creatures
-        render::CreaturesTabLayer charsLayer(*(moteur.getState().getCharacters().get()));
-        std::cout << "\nTID du thread lié à l'affichage : " << this_thread::get_id() << std::endl;
-        
-        std::cout << "Ici s'affrontent deux IAs heuristiques qui peuvent pour le moment se déplacer, combattre avec les quelques créatures qu'elles ont au départ de la partie, et ajouter de nouvelles creatures sur la carte." << std::endl;
-        std::cout << "Pour accelerer le processus, on permet aux IAs d'effectuer trois déplacements (qui peuvent aboutir à un combat) pendant leur phase d'attaque." << std::endl;
-        std::cout << "La démonstration se fera jusqu'à ce que la partie soit achevée." << std::endl;
-        
-        sf::Event event;
-        
-        // On tire au hasard le joueur qui commence en premier
-        beginner = rand()%2 + 1;
-        std::cout << "C'est l'IA n°" << beginner << " qui a ete tirée au sort pour debuter la partie." << std::endl;
-        
-        std::cout << "(APPUYER sur une touche de clavier pour lancer la partie.)" << std::endl;
-        
-        // On créé le thread lie à l'utilisation de l'ia et du moteur
-        thread threadIA(routine_thread,(void*)&ia);
-        
-        while (gameWindow.isOpen()) {
-            
-            while (gameWindow.pollEvent(event)) {
-                // Fermeture de la fenetre ?
-                if (event.type == sf::Event::Closed) gameWindow.close();
-                if (event.type == sf::Event::KeyPressed) std::cout << "\nTID du thread lié à l'affichage : " << this_thread::get_id() << std::endl;
-                notre_mutex.unlock();
-            }
-           
-            if (moteur.getState().getCellNbr() == moteur.getPlayer(1)->getCellNbr() || moteur.getPlayer(2)->getCellNbr() == 0)
-            {
-                cout << "L'IA n°1 a conquit toute la carte ou a totalement eliminé son adversaire !" << endl;
-                break;
-            }
-            
-            else if (moteur.getState().getCellNbr() == moteur.getPlayer(2)->getCellNbr() || moteur.getPlayer(1)->getCellNbr() == 0)
-            {
-                cout << "L'IA n°2 a conquit toute la carte ou a totalement eliminé son adversaire !" << endl;
-                break;
-            }
-            
-            // On met à jour l'affichage
-            cellLayer.initSurface();
-            charsLayer.initSurface();
-            
-            gameWindow.clear();
-            cellLayer.getSurface()->draw(gameWindow);
-            charsLayer.getSurface()->draw(gameWindow);
-            
-            gameWindow.display();
-        }
-        
-        // Attente de la fin du thread
-        threadIA.join();
-        std::cout << "\nNotre démonstration est terminée :)" << std::endl;
-    }
-    
-    void* routine_thread(void* ia)
-    {
-        HeuristicAI* adrIA = (HeuristicAI*) ia;
-        bool is_IA1_winner = (adrIA->getMoteur()->getState().getCellNbr() == adrIA->getMoteur()->getPlayer(1)->getCellNbr() || adrIA->getMoteur()->getPlayer(2)->getCellNbr() == 0);
-        bool is_IA2_winner = (adrIA->getMoteur()->getState().getCellNbr() == adrIA->getMoteur()->getPlayer(2)->getCellNbr() || adrIA->getMoteur()->getPlayer(1)->getCellNbr() == 0);
-        
-        // On reste dans cette boucle tant qu'aucun des deux joueurs n'a gagné la partie et que la fenetre est ouverte
-        while(!is_IA1_winner && !is_IA2_winner && gameWindow.isOpen())
-        {
-            std::lock_guard<std::mutex> lock (notre_mutex);
-            std::cout << "\nTID du thread lié à l'IA et au moteur : " << this_thread::get_id() << std::endl;
-            std::cout << "\n--------------    Tour n°" << tour / 2 + 1 << " - "; 
-            
-            // Tour de l'IA n°1
-            if (tour % 2 == 0) 
-            {
-                std::cout << "C'est à l'IA n°" << beginner << " de jouer    --------------" << std::endl << std::endl;
-                adrIA->run(beginner);
-            }
-            
-            // Tour de l'IA n°2
-            else
-            {
-                std::cout << "C'est à l'IA n°" << 3-beginner << " de jouer    --------------" << std::endl << std::endl;
-                adrIA->run(3-beginner);
-            }
-            
-            tour++;
-            adrIA->getMoteur()->increaseTour();
-            std::cout << "routine_thread - Points de victoire de l'IA n°1 : " << adrIA->getMoteur()->getPlayer(1)->getConquestPoints() << std::endl;
-            std::cout << "routine_thread - Points de victoire de l'IA n°2 : " << adrIA->getMoteur()->getPlayer(2)->getConquestPoints() << std::endl;
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            
-            // On verifie si l'un des deux joueurs a gagné ou non la partie
-            is_IA1_winner = (adrIA->getMoteur()->getState().getCellNbr() == adrIA->getMoteur()->getPlayer(1)->getCellNbr() || adrIA->getMoteur()->getPlayer(2)->getCellNbr() == 0);
-            is_IA2_winner = (adrIA->getMoteur()->getState().getCellNbr() == adrIA->getMoteur()->getPlayer(2)->getCellNbr() || adrIA->getMoteur()->getPlayer(1)->getCellNbr() == 0);
-            
-        }
-        
-        return 0;
-    }
+//    void TestsThread()
+//    {
+//        // On prend possession du mutex
+//        notre_mutex.lock();
+//        srand(time(NULL));
+//        
+//        // On initialise un moteur, on choisit les mineurs pour le joueur 1
+//        engine::Engine moteur(CreaturesID::MINERS);
+//        // On initialise une ia
+//        HeuristicAI ia(&moteur, rand()%30000);
+//        
+//        // On crée un Layer qui permettra de gerer l'affichage des cellules
+//        render::CellTabLayer cellLayer(*(moteur.getState().getGrid().get()));
+//        // On crée un Layer qui permettra de gerer l'affichage des creatures
+//        render::CreaturesTabLayer charsLayer(*(moteur.getState().getCharacters().get()));
+//        std::cout << "\nTID du thread lié à l'affichage : " << this_thread::get_id() << std::endl;
+//        
+//        std::cout << "Ici s'affrontent deux IAs heuristiques qui peuvent pour le moment se déplacer, combattre avec les quelques créatures qu'elles ont au départ de la partie, et ajouter de nouvelles creatures sur la carte." << std::endl;
+//        std::cout << "Pour accelerer le processus, on permet aux IAs d'effectuer trois déplacements (qui peuvent aboutir à un combat) pendant leur phase d'attaque." << std::endl;
+//        std::cout << "La démonstration se fera jusqu'à ce que la partie soit achevée." << std::endl;
+//        
+//        sf::Event event;
+//        
+//        // On tire au hasard le joueur qui commence en premier
+//        beginner = rand()%2 + 1;
+//        std::cout << "C'est l'IA n°" << beginner << " qui a ete tirée au sort pour debuter la partie." << std::endl;
+//        
+//        std::cout << "(APPUYER sur une touche de clavier pour lancer la partie.)" << std::endl;
+//        
+//        // On créé le thread lie à l'utilisation de l'ia et du moteur
+//        thread threadIA(routine_thread,(void*)&ia);
+//        
+//        while (gameWindow.isOpen()) {
+//            
+//            while (gameWindow.pollEvent(event)) {
+//                // Fermeture de la fenetre ?
+//                if (event.type == sf::Event::Closed) gameWindow.close();
+//                if (event.type == sf::Event::KeyPressed) std::cout << "\nTID du thread lié à l'affichage : " << this_thread::get_id() << std::endl;
+//                notre_mutex.unlock();
+//            }
+//           
+//            if (moteur.getState().getCellNbr() == moteur.getPlayer(1)->getCellNbr() || moteur.getPlayer(2)->getCellNbr() == 0)
+//            {
+//                cout << "L'IA n°1 a conquit toute la carte ou a totalement eliminé son adversaire !" << endl;
+//                break;
+//            }
+//            
+//            else if (moteur.getState().getCellNbr() == moteur.getPlayer(2)->getCellNbr() || moteur.getPlayer(1)->getCellNbr() == 0)
+//            {
+//                cout << "L'IA n°2 a conquit toute la carte ou a totalement eliminé son adversaire !" << endl;
+//                break;
+//            }
+//            
+//            // On met à jour l'affichage
+//            cellLayer.initSurface();
+//            charsLayer.initSurface();
+//            
+//            gameWindow.clear();
+//            cellLayer.getSurface()->draw(gameWindow);
+//            charsLayer.getSurface()->draw(gameWindow);
+//            
+//            gameWindow.display();
+//        }
+//        
+//        // Attente de la fin du thread
+//        threadIA.join();
+//        std::cout << "\nNotre démonstration est terminée :)" << std::endl;
+//    }
+//    
+//    void* routine_thread(void* ia)
+//    {
+//        HeuristicAI* adrIA = (HeuristicAI*) ia;
+//        bool is_IA1_winner = (adrIA->getMoteur()->getState().getCellNbr() == adrIA->getMoteur()->getPlayer(1)->getCellNbr() || adrIA->getMoteur()->getPlayer(2)->getCellNbr() == 0);
+//        bool is_IA2_winner = (adrIA->getMoteur()->getState().getCellNbr() == adrIA->getMoteur()->getPlayer(2)->getCellNbr() || adrIA->getMoteur()->getPlayer(1)->getCellNbr() == 0);
+//        
+//        // On reste dans cette boucle tant qu'aucun des deux joueurs n'a gagné la partie et que la fenetre est ouverte
+//        while(!is_IA1_winner && !is_IA2_winner && gameWindow.isOpen())
+//        {
+//            std::lock_guard<std::mutex> lock (notre_mutex);
+//            std::cout << "\nTID du thread lié à l'IA et au moteur : " << this_thread::get_id() << std::endl;
+//            std::cout << "\n--------------    Tour n°" << tour / 2 + 1 << " - "; 
+//            
+//            // Tour de l'IA n°1
+//            if (tour % 2 == 0) 
+//            {
+//                std::cout << "C'est à l'IA n°" << beginner << " de jouer    --------------" << std::endl << std::endl;
+//                adrIA->run(beginner);
+//            }
+//            
+//            // Tour de l'IA n°2
+//            else
+//            {
+//                std::cout << "C'est à l'IA n°" << 3-beginner << " de jouer    --------------" << std::endl << std::endl;
+//                adrIA->run(3-beginner);
+//            }
+//            
+//            tour++;
+//            adrIA->getMoteur()->increaseTour();
+//            std::cout << "routine_thread - Points de victoire de l'IA n°1 : " << adrIA->getMoteur()->getPlayer(1)->getConquestPoints() << std::endl;
+//            std::cout << "routine_thread - Points de victoire de l'IA n°2 : " << adrIA->getMoteur()->getPlayer(2)->getConquestPoints() << std::endl;
+//            std::this_thread::sleep_for(std::chrono::seconds(1));
+//            
+//            // On verifie si l'un des deux joueurs a gagné ou non la partie
+//            is_IA1_winner = (adrIA->getMoteur()->getState().getCellNbr() == adrIA->getMoteur()->getPlayer(1)->getCellNbr() || adrIA->getMoteur()->getPlayer(2)->getCellNbr() == 0);
+//            is_IA2_winner = (adrIA->getMoteur()->getState().getCellNbr() == adrIA->getMoteur()->getPlayer(2)->getCellNbr() || adrIA->getMoteur()->getPlayer(1)->getCellNbr() == 0);
+//            
+//        }
+//        
+//        return 0;
+//    }
     
 }
